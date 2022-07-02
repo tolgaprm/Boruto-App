ba33a829a181e10a3839bff839095e906c889333
Tolga Pirim <tolgapirim25@gmail.com>
2022-07-02 15:30:21 +0300

    create NetworkModules

---
+++ app/src/main/java/com/prmto/borutoapp/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.prmto.borutoapp.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.prmto.borutoapp.data.remote.BorutoApi
+import com.prmto.borutoapp.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@OptIn(ExperimentalSerializationApi::class)
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.MINUTES)
+            .connectTimeout(15, TimeUnit.MINUTES)
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        val contentType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoApi(retrofit: Retrofit): BorutoApi {
+        return retrofit.create(BorutoApi::class.java)
+    }
+}
--- app/src/main/java/com/prmto/borutoapp/util/Constants.kt
+++ app/src/main/java/com/prmto/borutoapp/util/Constants.kt
@@ -1,6 +1,9 @@
 package com.prmto.borutoapp.util
 
 object Constants {
+
+    const val BASE_URL ="http://10.0.2.2:8080"
+
     const val DETAILS_ARGUMENT_KEY = "heroId"
 
     const val HERO_DATABASE_TABLE = "hero_table"
